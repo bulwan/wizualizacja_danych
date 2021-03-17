0bec494fb62bc9d1f611663f228237d2ebf62eb7
bulwan <73537051+bulwan@users.noreply.github.com>
2021-03-17 17:26:52 +0100

    Add files via upload

--- lab_03_zadania/8.py
+++ lab_03_zadania/8.py
@@ -1,8 +1,12 @@
-def funkcja(a1, r, ile):
+def funkcja(a1=1, r=1, ile=10):
     suma = a1
     for x in range(1, ile, 1):
         suma = suma + (a1 + (x*r))
     return suma
 
-print(funkcja(1, 1, 10))
+
+a1 = int(input("Podaj a1: "))
+r = int(input("Podaj r: "))
+ile = int(input("Podaj ile elementow chcesz sumowac: "))
+print(funkcja(a1, r, ile))
 
